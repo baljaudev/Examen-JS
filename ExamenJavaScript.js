a46b963170afecad504679864f8ac2392bcbf7ec
baljaudev <r.baljau@gmail.com>
2022-04-30 23:24:11 +0200

    Depuración en JS

--- ExamenJavaScript.js
+++ ExamenJavaScript.js
@@ -1,20 +1,19 @@
-let numResCorrectas = 0
-let numResIncorrectas = 0
-let numResBlanco = 0
+let numResCorrectas = 0;
+let numResIncorrectas = 0;
+let numResBlanco = 0;
 
 function resultados() { 
+    let respuesta1 = "html";
+    let respuesta2 = "while";
+    let respuesta3 = "verdadero";
+    let respuesta4 = "interpretado";
+    let respuesta5 = "si";
 
-    let respuesta1 = "html"
-    let respuesta2 = "while"
-    let respuesta3 = "verdadero"
-    let respuesta4 = "interpretado"
-    let respuesta5 = "si"
-
-    let pregunta1 = returnChecked("pregunta1")
-    let pregunta2 = returnChecked("pregunta2")
-    let pregunta3 = returnChecked("pregunta3")
-    let pregunta4 = returnChecked("pregunta4")
-    let pregunta5 = returnChecked("pregunta5")
+    let pregunta1 = returnChecked("pregunta1");
+    let pregunta2 = returnChecked("pregunta2");
+    let pregunta3 = returnChecked("pregunta3");
+    let pregunta4 = returnChecked("pregunta4");
+    let pregunta5 = returnChecked("pregunta5");
 
     comprobar(pregunta1,respuesta1);
     comprobar(pregunta2,respuesta2);
@@ -22,25 +21,24 @@ function resultados() {
     comprobar(pregunta4,respuesta4);
     comprobar(pregunta5,respuesta5);
 
-    let nombre = document.getElementById("nombre").value
-    let apellidos = document.getElementById("apellidos").value
+    let nombre = document.getElementById("nombre").value;
+    let apellidos = document.getElementById("apellidos").value;
 
     if(nombre == "") {
-        alert("El nombre no puede estar vacío")
+        alert("El nombre no puede estar vacío");
     } else if(apellidos == "") {
-        alert("Los apellidos no pueden estar vacíos")
+        alert("Los apellidos no pueden estar vacíos");
     } else {
         alert("Nombre: " + nombre + 
         "\nApellidos: " + apellidos + 
         "\nNúmero de respuestas correctas: " + numResCorrectas +
         "\nNúmero de respuestas incorrectas: " + numResIncorrectas +
-        "\nNúmero de respuestas en blanco: " + numResBlanco)
+        "\nNúmero de respuestas en blanco: " + numResBlanco);
     }
 }
 
 
 function returnChecked(pregunta) {
-
     const radioButton = document.getElementsByName(pregunta);
     for(let i=0;i<radioButton.length;i++){
         if(radioButton[i].checked){
